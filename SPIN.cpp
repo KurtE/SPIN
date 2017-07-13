db5afa4989e3ba8fdf662572567b691a01500f35
Kurt Eckhardt <kurte@rockisland.com>
2017-07-13 08:14:54 -0700

    Make Spin one class now like SPI
    Make the SPIN class be one class, with three instances like SPI.  So it keeps a pointer to the SPI object and the like and all of the members are inline calls to the SPI function

--- SPIN.cpp
+++ SPIN.cpp
@@ -11,412 +11,62 @@
 #include "SPIN.h"
 #include "pins_arduino.h"
 
-////////////////////////
-/**********************************************************/
-/*   SPIN0                                                */
-/**********************************************************/
 
-void SPIN0Class::begin() {SPI.begin(); }
-void SPIN0Class::usingInterrupt(uint8_t n) {SPI.usingInterrupt(n); }
-void SPIN0Class::usingInterrupt(IRQ_NUMBER_t interruptName) {SPI.usingInterrupt(interruptName); }
-void SPIN0Class::notUsingInterrupt(IRQ_NUMBER_t interruptName) {SPI.notUsingInterrupt(interruptName); }
-
-// Before using SPI.transfer() or asserting chip select pins,
-// this function is used to gain exclusive access to the SPI bus
-// and configure the correct settings.
-void SPIN0Class::beginTransaction(SPISettings settings) {SPI.beginTransaction(settings);}
-
-// Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-uint8_t SPIN0Class::transfer(uint8_t data) {return SPI.transfer(data);}
-uint16_t SPIN0Class::transfer16(uint16_t data) {return SPI.transfer16(data);}
-void SPIN0Class::transfer(void *buf, size_t count) {SPI.transfer(buf, count); }
-
-// After performing a group of transfers and releasing the chip select
-// signal, this function allows others to access the SPI bus
-void SPIN0Class::endTransaction(void) {SPI.endTransaction();}
-void SPIN0Class::end() {SPI.end();}
-
-void SPIN0Class::setBitOrder(uint8_t bitOrder) {SPI.setBitOrder(bitOrder);}
-void SPIN0Class::setDataMode(uint8_t dataMode) {SPI.setDataMode(dataMode);}
-void SPIN0Class::setClockDivider(uint8_t clockDiv) {SPI.setClockDivider(clockDiv);}
-
-// Teensy 3.x can use alternate pins for these 3 SPI signals.
-void SPIN0Class::setMOSI(uint8_t pin) {SPI.setMOSI(pin);}
-void SPIN0Class::setMISO(uint8_t pin) {SPI.setMISO(pin);}
-void SPIN0Class::setSCK(uint8_t pin) {SPI.setSCK(pin);}
-
-bool SPIN0Class::pinIsMOSI(uint8_t pin)
-{
-    if ((pin == 11) || (pin == 7)) return true;
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    if (pin == 28) return true;
-#endif    
-    return false;
-}
-
-bool SPIN0Class::pinIsMISO(uint8_t pin)
-{
-    if ((pin == 12) || (pin == 8)) return true;
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    if (pin == 39) return true;
-#endif    
-    return false;
-}
-
-bool SPIN0Class::pinIsSCK(uint8_t pin)
-{
-    if ((pin == 13) || (pin == 14)) return true;
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    if (pin == 27) return true;
-#endif    
-    return false;
-}
-
-
-// return true if "pin" has special chip select capability
-uint8_t SPIN0Class::pinIsChipSelect(uint8_t pin) {return SPI.pinIsChipSelect(pin);}
-bool SPIN0Class::pinIsChipSelect(uint8_t pin1, uint8_t pin2) {return SPI.pinIsChipSelect(pin1, pin2);}
-uint8_t SPIN0Class::setCS(uint8_t pin) {return SPI.setCS(pin);}
-
-
-// Helper functions for managing queue
 #if defined(KINETISK)
-uint8_t SPIN0Class::sizeFIFO() {return 4;}
-
-KINETISK_SPI_t *SPIN0Class::kinetisk_spi (void) {
-    return &KINETISK_SPI0;
-}
-
-
-void SPIN0Class::waitFifoNotFull(void) {
+void SPINClass::waitFifoNotFull(void) {
     uint32_t sr;
     uint32_t tmp __attribute__((unused));
     do {
-        sr = KINETISK_SPI0.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI0.POPR;  // drain RX FIFO
-    } while ((sr & (15 << 12)) > ((4-1) << 12));
+        sr = port().SR;
+        if (sr & 0xF0) tmp = port().POPR;  // drain RX FIFO
+    } while ((uint32_t)(sr & (15 << 12)) > (uint32_t)((_fifo_size-1) << 12));
 }
-void SPIN0Class::waitFifoEmpty(void) {
+void SPINClass::waitFifoEmpty(void) {
     uint32_t sr;
     uint32_t tmp __attribute__((unused));
     do {
-        sr = KINETISK_SPI0.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI0.POPR;  // drain RX FIFO
+        sr = port().SR;
+        if (sr & 0xF0) tmp = port().POPR;  // drain RX FIFO
     } while ((sr & 0xF0F0) > 0);             // wait both RX & TX empty
 }
-void SPIN0Class::waitTransmitComplete(void)  {
+void SPINClass::waitTransmitComplete(void)  {
     uint32_t tmp __attribute__((unused));
-    while (!(KINETISK_SPI0.SR & SPI_SR_TCF)) ; // wait until final output done
-    tmp = KINETISK_SPI0.POPR;                  // drain the final RX FIFO word
+    while (!(port().SR & SPI_SR_TCF)) ; // wait until final output done
+    tmp = port().POPR;                  // drain the final RX FIFO word
 }
-void SPIN0Class::waitTransmitComplete(uint32_t mcr) {
-    uint32_t tmp __attribute__((unused));
-    while (1) {
-        uint32_t sr = KINETISK_SPI0.SR;
-        if (sr & SPI_SR_EOQF) break;  // wait for last transmit
-        if (sr &  0xF0) tmp = KINETISK_SPI0.POPR;
-    }
-    KINETISK_SPI0.SR = SPI_SR_EOQF;
-    KINETISK_SPI0.MCR = mcr;
-    while (KINETISK_SPI0.SR & 0xF0) {
-        tmp = KINETISK_SPI0.POPR;
-    }
-}
-
-uint8_t SPIN0Class::dmaTXEvent(void) {
-    return DMAMUX_SOURCE_SPI0_TX;
-
-}
-uint8_t SPIN0Class::dmaRXEvent(void) {
-    return DMAMUX_SOURCE_SPI0_RX;
-}
-
-#endif
-#if defined(KINETISL)
-KINETISL_SPI_t *SPIN0Class::kinetisl_spi (void) {
-    return &KINETISL_SPI0;
-}
-#endif    
-
-
-SPIN0Class SPIN;
-
-
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(KINETISL)
-/**********************************************************/
-/*   SPIN1                                               */
-/**********************************************************/
-
-void SPIN1Class::begin() {SPI1.begin(); }
-void SPIN1Class::usingInterrupt(uint8_t n) {SPI1.usingInterrupt(n); }
-void SPIN1Class::usingInterrupt(IRQ_NUMBER_t interruptName) {SPI1.usingInterrupt(interruptName); }
-void SPIN1Class::notUsingInterrupt(IRQ_NUMBER_t interruptName) {SPI1.notUsingInterrupt(interruptName); }
-
-// Before using SPI1.transfer() or asserting chip select pins,
-// this function is used to gain exclusive access to the SPI bus
-// and configure the correct settings.
-void SPIN1Class::beginTransaction(SPISettings settings) {SPI1.beginTransaction(settings);}
-
-// Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-uint8_t SPIN1Class::transfer(uint8_t data) {return SPI1.transfer(data);}
-uint16_t SPIN1Class::transfer16(uint16_t data) {return SPI1.transfer16(data);}
-void SPIN1Class::transfer(void *buf, size_t count) {SPI1.transfer(buf, count); }
-
-// After performing a group of transfers and releasing the chip select
-// signal, this function allows others to access the SPI bus
-void SPIN1Class::endTransaction(void) {SPI1.endTransaction();}
-void SPIN1Class::end() {SPI1.end();}
-
-void SPIN1Class::setBitOrder(uint8_t bitOrder) {SPI1.setBitOrder(bitOrder);}
-void SPIN1Class::setDataMode(uint8_t dataMode) {SPI1.setDataMode(dataMode);}
-void SPIN1Class::setClockDivider(uint8_t clockDiv) {SPI1.setClockDivider(clockDiv);}
-
-// Teensy 3.x can use alternate pins for these 3 SPI signals.
-void SPIN1Class::setMOSI(uint8_t pin) {SPI1.setMOSI(pin);}
-void SPIN1Class::setMISO(uint8_t pin) {SPI1.setMISO(pin);}
-void SPIN1Class::setSCK(uint8_t pin) {SPI1.setSCK(pin);}
-bool SPIN1Class::pinIsMOSI(uint8_t pin)
-{
-    switch (pin) {
-    case 0: 
-    case 21:
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    case 61:
-    case 59:
-#endif
-        //Serial.printf("Valid MOSI %d\n\r", pin);
-        return true;
-    }
-    return false;
-
-}
-
-bool SPIN1Class::pinIsMISO(uint8_t pin)
-{
-    switch (pin) {
-    case 1: 
-    case 5: 
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    case 61:
-    case 59:
-#endif
-        //Serial.printf("Valid MISO %d\n\r", pin);
-        return true;
-    }
-    return false;
-}
-
-bool SPIN1Class::pinIsSCK(uint8_t pin)
-{
-    switch (pin) {
-    case 20:
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-    case 32:
-    case 60: 
-#endif
-        //Serial.printf("Valid SCK %d\n\r", pin);
-        return true;
-    }
-    return false;
-}
-
-// return true if "pin" has special chip select capability
-uint8_t SPIN1Class::pinIsChipSelect(uint8_t pin) {return SPI1.pinIsChipSelect(pin);}
-bool SPIN1Class::pinIsChipSelect(uint8_t pin1, uint8_t pin2) {return SPI1.pinIsChipSelect(pin1, pin2);}
-uint8_t SPIN1Class::setCS(uint8_t pin) {return SPI1.setCS(pin);}
-
-// Helper functions for managing queue
-#if defined(KINETISK)
-uint8_t SPIN1Class::sizeFIFO() {return 1;}
-KINETISK_SPI_t *SPIN1Class::kinetisk_spi (void) {
-    return &KINETISK_SPI1;
-}
-
-
-void SPIN1Class::waitFifoNotFull(void) {
-    uint32_t sr;
-    uint32_t tmp __attribute__((unused));
-    do {
-        sr = KINETISK_SPI1.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI1.POPR;  // drain RX FIFO
-    } while ((sr & (15 << 12)) > ((1-1) << 12));
-}
-void SPIN1Class::waitFifoEmpty(void) {
-    uint32_t sr;
-    uint32_t tmp __attribute__((unused));
-    do {
-        sr = KINETISK_SPI1.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI1.POPR;  // drain RX FIFO
-    } while ((sr & 0xF0F0) > 0);             // wait both RX & TX empty
-}
-void SPIN1Class::waitTransmitComplete(void) {
-    uint32_t tmp __attribute__((unused));
-    while (!(KINETISK_SPI1.SR & SPI_SR_TCF)) ; // wait until final output done
-    tmp = KINETISK_SPI1.POPR;                  // drain the final RX FIFO word
-}
-void SPIN1Class::waitTransmitComplete(uint32_t mcr)  {
+void SPINClass::waitTransmitComplete(uint32_t mcr) {
     uint32_t tmp __attribute__((unused));
     while (1) {
-        uint32_t sr = KINETISK_SPI1.SR;
+        uint32_t sr = port().SR;
         if (sr & SPI_SR_EOQF) break;  // wait for last transmit
-        if (sr &  0xF0) tmp = KINETISK_SPI1.POPR;
+        if (sr &  0xF0) tmp = port().POPR;
     }
-    KINETISK_SPI1.SR = SPI_SR_EOQF;
-    KINETISK_SPI1.MCR = mcr;
-    while (KINETISK_SPI1.SR & 0xF0) {
-        tmp = KINETISK_SPI1.POPR;
-    }
-}
-uint8_t SPIN1Class::dmaTXEvent(void) {
-    return DMAMUX_SOURCE_SPI1_TX;
-
-}
-uint8_t SPIN1Class::dmaRXEvent(void) {
-    return DMAMUX_SOURCE_SPI1_RX;
-}
-#endif
-
-#if defined(KINETISL)
-KINETISL_SPI_t *SPIN1Class::kinetisl_spi (void) {
-    return &KINETISL_SPI1;
-}
-#endif    
-
-
-SPIN1Class SPIN1;
-#endif
-
-
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-/**********************************************************/
-/*   SPIN2                                                */
-/**********************************************************/
-
-void SPIN2Class::begin() {SPI2.begin(); }
-void SPIN2Class::usingInterrupt(uint8_t n) {SPI2.usingInterrupt(n); }
-void SPIN2Class::usingInterrupt(IRQ_NUMBER_t interruptName) {SPI2.usingInterrupt(interruptName); }
-void SPIN2Class::notUsingInterrupt(IRQ_NUMBER_t interruptName) {SPI2.notUsingInterrupt(interruptName); }
-
-// Before using SPI2.transfer() or asserting chip select pins,
-// this function is used to gain exclusive access to the SPI bus
-// and configure the correct settings.
-void SPIN2Class::beginTransaction(SPISettings settings) {SPI2.beginTransaction(settings);}
-
-// Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-uint8_t SPIN2Class::transfer(uint8_t data) {return SPI2.transfer(data);}
-uint16_t SPIN2Class::transfer16(uint16_t data) {return SPI2.transfer16(data);}
-void SPIN2Class::transfer(void *buf, size_t count) {SPI2.transfer(buf, count); }
-
-// After performing a group of transfers and releasing the chip select
-// signal, this function allows others to access the SPI bus
-void SPIN2Class::endTransaction(void) {SPI2.endTransaction();}
-void SPIN2Class::end() {SPI2.end();}
-
-void SPIN2Class::setBitOrder(uint8_t bitOrder) {SPI2.setBitOrder(bitOrder);}
-void SPIN2Class::setDataMode(uint8_t dataMode) {SPI2.setDataMode(dataMode);}
-void SPIN2Class::setClockDivider(uint8_t clockDiv) {SPI2.setClockDivider(clockDiv);}
-
-// Teensy 3.x can use alternate pins for these 3 SPI signals.
-void SPIN2Class::setMOSI(uint8_t pin) {SPI2.setMOSI(pin);}
-void SPIN2Class::setMISO(uint8_t pin) {SPI2.setMISO(pin);}
-void SPIN2Class::setSCK(uint8_t pin) {SPI2.setSCK(pin);}
-bool SPIN2Class::pinIsMOSI(uint8_t pin)
-{
-    switch (pin) {
-    case 44: 
-    case 52:
-        return true;
-    }
-    return false;
-
-}
-
-bool SPIN2Class::pinIsMISO(uint8_t pin)
-{
-    switch (pin) {
-    case 45: 
-    case 51: 
-        return true;
-    }
-    return false;
-}
-
-bool SPIN2Class::pinIsSCK(uint8_t pin)
-{
-    switch (pin) {
-    case 46:
-    case 53:
-        return true;
-    }
-    return false;
-}
-
-// return true if "pin" has special chip select capability
-uint8_t SPIN2Class::pinIsChipSelect(uint8_t pin) {return SPI2.pinIsChipSelect(pin);}
-bool SPIN2Class::pinIsChipSelect(uint8_t pin1, uint8_t pin2) {return SPI2.pinIsChipSelect(pin1, pin2);}
-uint8_t SPIN2Class::setCS(uint8_t pin) {return SPI2.setCS(pin);}
-
-// Helper functions for managing queue
-#if defined(KINETISK)
-uint8_t SPIN2Class::sizeFIFO() {return 1;}
-KINETISK_SPI_t *SPIN2Class::kinetisk_spi (void) {
-    return &KINETISK_SPI2;
-}
-
-
-void SPIN2Class::waitFifoNotFull(void) {
-    uint32_t sr;
-    uint32_t tmp __attribute__((unused));
-    do {
-        sr = KINETISK_SPI2.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI2.POPR;  // drain RX FIFO
-    } while ((sr & (15 << 12)) > ((1-1) << 12));
-}
-void SPIN2Class::waitFifoEmpty(void) {
-    uint32_t sr;
-    uint32_t tmp __attribute__((unused));
-    do {
-        sr = KINETISK_SPI2.SR;
-        if (sr & 0xF0) tmp = KINETISK_SPI2.POPR;  // drain RX FIFO
-    } while ((sr & 0xF0F0) > 0);             // wait both RX & TX empty
-}
-void SPIN2Class::waitTransmitComplete(void) {
-    uint32_t tmp __attribute__((unused));
-    while (!(KINETISK_SPI2.SR & SPI_SR_TCF)) ; // wait until final output done
-    tmp = KINETISK_SPI2.POPR;                  // drain the final RX FIFO word
-}
-void SPIN2Class::waitTransmitComplete(uint32_t mcr)  {
-    uint32_t tmp __attribute__((unused));
-    while (1) {
-        uint32_t sr = KINETISK_SPI2.SR;
-        if (sr & SPI_SR_EOQF) break;  // wait for last transmit
-        if (sr &  0xF0) tmp = KINETISK_SPI2.POPR;
-    }
-    KINETISK_SPI2.SR = SPI_SR_EOQF;
-    KINETISK_SPI2.MCR = mcr;
-    while (KINETISK_SPI2.SR & 0xF0) {
-        tmp = KINETISK_SPI2.POPR;
+    port().SR = SPI_SR_EOQF;
+    port().MCR = mcr;
+    while (port().SR & 0xF0) {
+        tmp = port().POPR;
     }
 }
 
-uint8_t SPIN2Class::dmaTXEvent(void) {
-#ifdef DMAMUX_SOURCE_SPI2_TX
-    return DMAMUX_SOURCE_SPI2_TX;
+
+SPINClass SPIN((uintptr_t)&SPI, (uintptr_t)&KINETISK_SPI0, 4, DMAMUX_SOURCE_SPI0_TX, DMAMUX_SOURCE_SPI0_RX);
 #else
-    return (uint8_t)-1;
-#endif
-}
-
-uint8_t SPIN2Class::dmaRXEvent(void) {
-#ifdef DMAMUX_SOURCE_SPI2_RX
-    return DMAMUX_SOURCE_SPI2_RX;
-#else
-    return (uint8_t)-1;
-#endif
-}
-
+SPINClass SPIN((uintptr_t)&SPI, (uintptr_t)&KINETISL_SPI0);
 #endif
 
+// Define SPI1 and SPI2 for those boards who support them. 
+#if defined(__MK64FX512__) 
+// Teensy 3.5
+// Warning T3.5 has only one DMAMUX source for SPI1 and SPI2 which can be RX or TX 
+SPINClass SPIN1((uintptr_t)&SPI1, (uintptr_t)&KINETISK_SPI1, 1, DMAMUX_SOURCE_SPI1, DMAMUX_SOURCE_SPI1);
+SPINClass SPIN2((uintptr_t)&SPI2, (uintptr_t)&KINETISK_SPI2, 1, DMAMUX_SOURCE_SPI2, DMAMUX_SOURCE_SPI2);
 
-SPIN2Class SPIN2;
+#elif defined(__MK66FX1M0__) 
+// Teensy 3.6
+SPINClass SPIN1((uintptr_t)&SPI1, (uintptr_t)&KINETISK_SPI1, 1, DMAMUX_SOURCE_SPI1_TX, DMAMUX_SOURCE_SPI1_RX);
+SPINClass SPIN2((uintptr_t)&SPI2, (uintptr_t)&KINETISK_SPI2, 1, DMAMUX_SOURCE_SPI2_TX, DMAMUX_SOURCE_SPI2_RX);
+
+#elif defined(KINETISL)
+// Teensy LC
+SPINClass SPIN1((uintptr_t)&SPI1, (uintptr_t)&KINETISL_SPI1);
 #endif
--- SPIN.h
+++ SPIN.h
@@ -24,250 +24,99 @@
 class SPINClass {
 public:
     // Initialize the SPI library
-    virtual void begin() = 0;
-    virtual void usingInterrupt(uint8_t n) = 0;
-    virtual void usingInterrupt(IRQ_NUMBER_t interruptName) = 0;
-    virtual void notUsingInterrupt(IRQ_NUMBER_t interruptName) = 0;
+    inline void begin() {spi().begin(); }
+    inline void usingInterrupt(uint8_t n) {spi().usingInterrupt(n); }
+    inline void usingInterrupt(IRQ_NUMBER_t interruptName) {spi().usingInterrupt(interruptName); }
+    inline void notUsingInterrupt(IRQ_NUMBER_t interruptName) {spi().notUsingInterrupt(interruptName); }
 
     // Before using SPI.transfer() or asserting chip select pins,
     // this function is used to gain exclusive access to the SPI bus
     // and configure the correct settings.
-    virtual void beginTransaction(SPISettings settings) = 0;
+    inline void beginTransaction(SPISettings settings)  {spi().beginTransaction(settings);}
 
     // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-    virtual uint8_t transfer(uint8_t data) = 0;
-    virtual uint16_t transfer16(uint16_t data) = 0;
-    virtual void transfer(void *buf, size_t count) = 0;
+    inline uint8_t transfer(uint8_t data) {return spi().transfer(data);}
+    inline uint16_t transfer16(uint16_t data) {return spi().transfer16(data);}
+    inline void transfer(void *buf, size_t count) {spi().transfer(buf, count); }
 
     // After performing a group of transfers and releasing the chip select
     // signal, this function allows others to access the SPI bus
-    virtual void endTransaction(void) = 0;
-    virtual void end() = 0;
+    inline void endTransaction(void)  {spi().endTransaction();}
+    inline void end() {spi().end();}
 
-    virtual void setBitOrder(uint8_t bitOrder) = 0;
-    virtual void setDataMode(uint8_t dataMode) = 0;
-    virtual void setClockDivider(uint8_t clockDiv) = 0;
+    inline void setBitOrder(uint8_t bitOrder) {spi().setBitOrder(bitOrder);}
+    inline void setDataMode(uint8_t dataMode) {spi().setDataMode(dataMode);}
+    inline void setClockDivider(uint8_t clockDiv) {spi().setClockDivider(clockDiv);}
 
     // Teensy 3.x can use alternate pins for these 3 SPI signals.
-    virtual void setMOSI(uint8_t pin) = 0;
-    virtual void setMISO(uint8_t pin) = 0;
-    virtual void setSCK(uint8_t pin) = 0;
+    inline void setMOSI(uint8_t pin)  {spi().setMOSI(pin);}
+    inline void setMISO(uint8_t pin) {spi().setMISO(pin);}
+    inline void setSCK(uint8_t pin) {spi().setSCK(pin);}
     // Added ones that are not part of SPI (yet)
-    virtual bool pinIsMOSI(uint8_t pin) = 0;
-    virtual bool pinIsMISO(uint8_t pin) = 0;
-    virtual bool pinIsSCK(uint8_t pin) = 0;
+    inline bool pinIsMOSI(uint8_t pin)  {return spi().pinIsMOSI(pin);}
+    inline bool pinIsMISO(uint8_t pin) {return spi().pinIsMISO(pin);}
+    inline bool pinIsSCK(uint8_t pin) {return spi().pinIsSCK(pin);}
 
     // return true if "pin" has special chip select capability
-    virtual uint8_t pinIsChipSelect(uint8_t pin) = 0;
-    virtual bool pinIsChipSelect(uint8_t pin1, uint8_t pin2) = 0;
-    virtual uint8_t setCS(uint8_t pin) = 0;
+    inline uint8_t pinIsChipSelect(uint8_t pin) {return spi().pinIsChipSelect(pin);}
+    inline bool pinIsChipSelect(uint8_t pin1, uint8_t pin2) {return spi().pinIsChipSelect(pin1, pin2);}
+    inline uint8_t setCS(uint8_t pin) {return spi().setCS(pin);}
 
     // Add helper functions from ILI9341, which manage SPI FIFO
 #if defined(KINETISK)
-    virtual KINETISK_SPI_t *kinetisk_spi (void) = 0;
+constexpr SPINClass(uintptr_t spiX, uintptr_t myport, uint8_t fifo_size, uint8_t dma_tx_event, uint8_t dma_rx_event)
+        : _spi(spiX), _port_addr(myport), _fifo_size(fifo_size), 
+        _dma_tx_event(dma_tx_event), _dma_rx_event(dma_rx_event) {
+    }
 
-    virtual uint8_t sizeFIFO() = 0;
-    virtual void waitFifoNotFull(void) = 0;
-    virtual void waitFifoEmpty(void) = 0;
-    virtual void waitTransmitComplete(void)  = 0;
-    virtual void waitTransmitComplete(uint32_t mcr) = 0;
+    inline KINETISK_SPI_t & port() { return *(KINETISK_SPI_t *)_port_addr; }
+
+    inline uint8_t sizeFIFO() {return _fifo_size; }
+    void waitFifoNotFull(void);
+    void waitFifoEmpty(void);
+    void waitTransmitComplete(void) ;
+    void waitTransmitComplete(uint32_t mcr);
 
     // return DMA Channel information 
-    virtual uint8_t dmaTXEvent(void) = 0;
-    virtual uint8_t dmaRXEvent(void) = 0;
+    inline uint8_t dmaTXEvent(void) {return _dma_tx_event;}
+    inline uint8_t dmaRXEvent(void) {return _dma_rx_event;}
+    inline SPIClass & spi() { return *(SPIClass *)_spi;}
 #endif
 #if defined(KINETISL)
-    virtual KINETISL_SPI_t *kinetisl_spi (void);
+constexpr SPINClass(uintptr_t spiX, uintptr_t myport)
+        : _spi(spiX), _port_addr(myport) {
+    }
+
+    inline KINETISL_SPI_t & port() { return *(KINETISL_SPI_t *)_port_addr; }
+    inline SPIClass & spi() { return *(SPIClass *)_spi;}
 #endif    
-};
 
-/**********************************************************/
-/*   SPIN0                                                */
-/**********************************************************/
-class SPIN0Class : public SPINClass {
-public:
-    // Initialize the SPI library
-    virtual void begin();
-    virtual void usingInterrupt(uint8_t n);
-    virtual void usingInterrupt(IRQ_NUMBER_t interruptName);
-    virtual void notUsingInterrupt(IRQ_NUMBER_t interruptName);
-
-    // Before using SPI.transfer() or asserting chip select pins,
-    // this function is used to gain exclusive access to the SPI bus
-    // and configure the correct settings.
-    virtual void beginTransaction(SPISettings settings);
-
-    // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-    virtual uint8_t transfer(uint8_t data);
-    virtual uint16_t transfer16(uint16_t data);
-    virtual void transfer(void *buf, size_t count);
-
-    // After performing a group of transfers and releasing the chip select
-    // signal, this function allows others to access the SPI bus
-    virtual void endTransaction(void);
-    virtual void end();
-
-    virtual void setBitOrder(uint8_t bitOrder);
-    virtual void setDataMode(uint8_t dataMode);
-    virtual void setClockDivider(uint8_t clockDiv);
-
-    // Teensy 3.x can use alternate pins for these 3 SPI signals.
-    virtual void setMOSI(uint8_t pin);
-    virtual void setMISO(uint8_t pin);
-    virtual void setSCK(uint8_t pin);
-    virtual bool pinIsMOSI(uint8_t pin);
-    virtual bool pinIsMISO(uint8_t pin);
-    virtual bool pinIsSCK(uint8_t pin);
-
-    // return true if "pin" has special chip select capability
-    virtual uint8_t pinIsChipSelect(uint8_t pin);
-    virtual bool pinIsChipSelect(uint8_t pin1, uint8_t pin2);
-    virtual uint8_t setCS(uint8_t pin);
-
+private:
+    uintptr_t _spi;
 #if defined(KINETISK)
-    virtual KINETISK_SPI_t *kinetisk_spi (void);
 
-    virtual uint8_t sizeFIFO();
-    virtual void waitFifoNotFull(void);
-    virtual void waitFifoEmpty(void);
-    virtual void waitTransmitComplete(void);
-    virtual void waitTransmitComplete(uint32_t mcr);
-    virtual uint8_t dmaTXEvent(void);
-    virtual uint8_t dmaRXEvent(void);
-#endif
-#if defined(KINETISL)
-    virtual KINETISL_SPI_t *kinetisl_spi (void);
-#endif    
+    uintptr_t _port_addr;
+    uint8_t _fifo_size;
+    uint8_t _dma_tx_event;
+    uint8_t _dma_rx_event;
+#else
+    uintptr_t _port_addr;
+#endif        
+
 };
 
+extern SPINClass SPIN;
 
 #if defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(KINETISL)
 #define SPIN1_OBJECT_CREATED
-/**********************************************************/
-/*   SPIN1                                                */
-/**********************************************************/
-class SPIN1Class : public SPINClass {
-public:
-    // Initialize the SPI library
-    virtual void begin();
-    virtual void usingInterrupt(uint8_t n);
-    virtual void usingInterrupt(IRQ_NUMBER_t interruptName);
-    virtual void notUsingInterrupt(IRQ_NUMBER_t interruptName);
-
-    // Before using SPI.transfer() or asserting chip select pins,
-    // this function is used to gain exclusive access to the SPI bus
-    // and configure the correct settings.
-    virtual void beginTransaction(SPISettings settings);
-
-    // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-    virtual uint8_t transfer(uint8_t data);
-    virtual uint16_t transfer16(uint16_t data);
-    virtual void transfer(void *buf, size_t count);
-
-    // After performing a group of transfers and releasing the chip select
-    // signal, this function allows others to access the SPI bus
-    virtual void endTransaction(void);
-    virtual void end();
-
-    virtual void setBitOrder(uint8_t bitOrder);
-    virtual void setDataMode(uint8_t dataMode);
-    virtual void setClockDivider(uint8_t clockDiv);
-
-    // Teensy 3.x can use alternate pins for these 3 SPI signals.
-    virtual void setMOSI(uint8_t pin);
-    virtual void setMISO(uint8_t pin);
-    virtual void setSCK(uint8_t pin);
-    virtual bool pinIsMOSI(uint8_t pin);
-    virtual bool pinIsMISO(uint8_t pin);
-    virtual bool pinIsSCK(uint8_t pin);
-
-    // return true if "pin" has special chip select capability
-    virtual uint8_t pinIsChipSelect(uint8_t pin);
-    virtual bool pinIsChipSelect(uint8_t pin1, uint8_t pin2);
-    virtual uint8_t setCS(uint8_t pin);
-
-#if defined(KINETISK)
-    virtual KINETISK_SPI_t *kinetisk_spi (void);
-
-    virtual uint8_t sizeFIFO();
-    virtual void waitFifoNotFull(void);
-    virtual void waitFifoEmpty(void);
-    virtual void waitTransmitComplete(void);
-    virtual void waitTransmitComplete(uint32_t mcr);
-    virtual uint8_t dmaTXEvent(void);
-    virtual uint8_t dmaRXEvent(void);
-#endif
-#if defined(KINETISL)
-    virtual KINETISL_SPI_t *kinetisl_spi (void);
-#endif    
-};
-
-extern SPIN1Class SPIN1;
+extern SPINClass SPIN1;
 #endif
 
 
 #if defined(__MK64FX512__) || defined(__MK66FX1M0__)
 #define SPIN2_OBJECT_CREATED
-/**********************************************************/
-/*   SPIN2                                                */
-/**********************************************************/
-class SPIN2Class : public SPINClass {
-public:
-    // Initialize the SPI library
-    virtual void begin();
-    virtual void usingInterrupt(uint8_t n);
-    virtual void usingInterrupt(IRQ_NUMBER_t interruptName);
-    virtual void notUsingInterrupt(IRQ_NUMBER_t interruptName);
-
-    // Before using SPI.transfer() or asserting chip select pins,
-    // this function is used to gain exclusive access to the SPI bus
-    // and configure the correct settings.
-    virtual void beginTransaction(SPISettings settings);
-
-    // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
-    virtual uint8_t transfer(uint8_t data);
-    virtual uint16_t transfer16(uint16_t data);
-    virtual void transfer(void *buf, size_t count);
-
-    // After performing a group of transfers and releasing the chip select
-    // signal, this function allows others to access the SPI bus
-    virtual void endTransaction(void);
-    virtual void end();
-
-    virtual void setBitOrder(uint8_t bitOrder);
-    virtual void setDataMode(uint8_t dataMode);
-    virtual void setClockDivider(uint8_t clockDiv);
-
-    // Teensy 3.x can use alternate pins for these 3 SPI signals.
-    virtual void setMOSI(uint8_t pin);
-    virtual void setMISO(uint8_t pin);
-    virtual void setSCK(uint8_t pin);
-    virtual bool pinIsMOSI(uint8_t pin);
-    virtual bool pinIsMISO(uint8_t pin);
-    virtual bool pinIsSCK(uint8_t pin);
-
-    // return true if "pin" has special chip select capability
-    virtual uint8_t pinIsChipSelect(uint8_t pin);
-    virtual bool pinIsChipSelect(uint8_t pin1, uint8_t pin2);
-    virtual uint8_t setCS(uint8_t pin);
-
-#if defined(KINETISK)
-    virtual KINETISK_SPI_t *kinetisk_spi (void);
-
-    virtual uint8_t sizeFIFO();
-    virtual void waitFifoNotFull(void);
-    virtual void waitFifoEmpty(void);
-    virtual void waitTransmitComplete(void);
-    virtual void waitTransmitComplete(uint32_t mcr);
-    virtual uint8_t dmaTXEvent(void);
-    virtual uint8_t dmaRXEvent(void);
-#endif
-};
-
-
-extern SPIN2Class SPIN2;
+extern SPINClass SPIN2;
 #endif
 
-extern SPIN0Class SPIN;
 #endif
 #endif
